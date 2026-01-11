ee8c537d06c6b98e3f0612091789eed60f67f974
Harsh-2005d <dahiyaharsh2005@gmail.com>
2026-01-11 22:42:50 +0530

    route

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
-import { StrictMode } from 'react'
+// import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 // import App from './App.tsx'
-import Profile from './components/Profile.tsx'
+import AppRouter from './routes/AppRouter'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <Profile />
-  </StrictMode>,
+  <BrowserRouter>
+    <AppRouter />
+  </BrowserRouter>
 )
--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,11 +1,12 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import Dashboard from "../pages/Dashboard";
+import ProfilePage from "../components/Profile";
 
 const AppRouter = () => {
   return (
     <Routes>
-      <Route path="/" element={<Navigate to="/dashboard" replace />} />
-      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/" element={<Dashboard />} />
+      <Route path="/profile" element={<ProfilePage />} />
     </Routes>
   );
 };
