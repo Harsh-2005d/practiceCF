dda08eb4ebad35bcca7490d1d67e31302f3596ae
Harsh-2005d <dahiyaharsh2005@gmail.com>
2026-01-11 23:18:34 +0530

    skeleton done

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 // import App from './App.tsx'
+import Navbar from './components/Navbar'
 import AppRouter from './routes/AppRouter'
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
+    <Navbar />
     <AppRouter />
   </BrowserRouter>
 )
