884c60607c6c659149fc345477e1b67e57f8819f
iHamza14 <anwarhamza1408@gmail.com>
2026-01-21 00:27:14 +0530

    added graph

--- backend/src/routes/revise.route.ts
+++ backend/src/routes/revise.route.ts
@@ -2,10 +2,13 @@ import { Router } from "express";
 import { checkAuth } from "../middleware/auth.middleware";
 import { reviseController } from "../controllers/revise.controller";
 import { setHandleController } from "../controllers/setHandler.controller";
+import { histogramController } from "../controllers/stats.controller";
 const router = Router();
 
 router.post("/handle", checkAuth, setHandleController);
 router.get("/revise", checkAuth, reviseController);
+router.get("/stats/histogram", checkAuth, histogramController);
+
 
 
 export default router;
